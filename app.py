91f64615e20ea2b294cdfb035feb2c201c789107
Cristiananc <cristiananc@hotmail.com>
2020-12-04 04:58:40 -0300

    adding json network file

--- app.py
+++ app.py
@@ -8,20 +8,54 @@ server = app.server
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
+with open('producaoalimentos.json', 'r') as f:
+    data = json.loads(f.read())
+    
+with open('cy-style.json') as f:
+    stylesheet = json.loads(f.read())
+
+
 styles = {}
 
 app.layout = html.Div([
     html.H2('Produção de Alimentos na América Latina'),
-    html.H2('Teste'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['2015','2016', '2017']],
         value='2015'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+    html.Div(className='cy-container', style=styles['cy-container'], children=[
+        cyto.Cytoscape(
+            id='cytoscape',
+            elements=data['elements'],
+            stylesheet=stylesheet,
+            style=styles['cytoscape'],
+            layout={
+                'name': 'cose',
+                'idealEdgeLength': 100,
+                'nodeOverlap': 20,
+                'refresh': 20,
+                'fit': True,
+                'padding': 30,
+                'randomize': False,
+                'componentSpacing': 100,
+                'nodeRepulsion': 400000,
+                'edgeElasticity': 100,
+                'nestingFactor': 5,
+                'gravity': 80,
+                'numIter': 1000,
+                'initialTemp': 200,
+                'coolingFactor': 0.95,
+                'minTemp': 1.0
+            },
+            responsive=True
+        )
+    ])
 ])
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
+
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 if __name__ == '__main__':
